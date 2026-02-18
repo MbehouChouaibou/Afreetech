10a2ebd85ee0d386e1e31c92ff5fa1625601f1e1
EVERMATE <chouaiboumbehou@gmail.com>
2026-02-18 18:30:00 +0100

    last commit

--- initia-frontend/src/types/ClientTypes.ts
+++ initia-frontend/src/types/ClientTypes.ts
@@ -8,3 +8,12 @@ export interface ClientDTO {
   email: string;
   assuranceIds?: number[];   // ← ajouté comme dans ton exemple JSON
 }
+export interface ClientCreateDTO {
+  nom: string;
+  prenom: string;
+  dateNaissance?: string;      // "YYYY-MM-DD"
+  adresse?: string;
+  telephone?: string;
+  email: string;
+  assuranceIds?: number[];     // tableau vide ou avec IDs
+}
